b301e76548c2c4af8e49fedd643b7ce61a6ec643
EntoriNashi <110409000+EntoriNashi@users.noreply.github.com>
2023-05-10 18:20:21 -0700

    Basic Enemy Setup

---
+++ ToughCatsProject/Assets/Scripts/enemyAI.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyAI : MonoBehaviour, IDamage
+{
+
+    [SerializeFeild] int HP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Public void takeDamage(int dmg)
+    {
+        HP -= dmg;
+
+        if (HP <= 0) 
+        {
+            Destroy(gameObject);
+        }
+    }
+}
