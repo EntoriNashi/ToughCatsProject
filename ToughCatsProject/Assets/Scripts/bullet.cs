100d9d09402b8dc8a7713caba1d9917255cf67bb
EntoriNashi <110409000+EntoriNashi@users.noreply.github.com>
2023-05-10 22:19:09 -0700

    Bullet and Enemy Shooting

---
+++ ToughCatsProject/Assets/Scripts/bullet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    [SerializeField] int damage;
+    [SerializeField] int speed;
+    [SerializeField] int timer;
+
+    [SerializeField] Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, timer);
+        rb.velocity = transform.forward * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IDamage damagable = other.GetComponent<IDamage>();
+        if (damagable != null) 
+        {
+            damagable.takeDamage(damage);
+        }
+
+        Destroy(gameObject);
+    }
+}
--- ToughCatsProject/Assets/Scripts/enemyAI.cs
+++ ToughCatsProject/Assets/Scripts/enemyAI.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class enemyAI : MonoBehaviour, IDamage
 {
-
+    [Header("----- Components -----")]
     [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
 
+    [Header("----- Enemy Stats -----")]
     [SerializeField] int HP;
 
+    [Header("----- Enemy Weapon -----")]
+    [Range(2,300)][SerializeField] int shootDist;
+    [Range(0.1f, 3)][SerializeField] float shootRate;
+    [SerializeField] GameObject bullet;
+
     Color colorOrig;
+    bool isShooting;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +29,9 @@ public class enemyAI : MonoBehaviour, IDamage
     // Update is called once per frame
     void Update()
     {
-       
+       agent.SetDestination(gameManager.instance.player.transform.position);
+        if (!isShooting)
+            StartCoroutine(shoot());
     }
 
     public void takeDamage(int dmg)
@@ -36,8 +47,16 @@ public class enemyAI : MonoBehaviour, IDamage
     
     IEnumerator flashColor()
     {
-        model.material.color = Color.Red;
+        model.material.color = Color.red;
         yield return new WaitForSeconds(0.1f);
         model.material.color = colorOrig;
+    }
+
+    IEnumerator shoot()
+    {
+        isShooting = true;
+        Instantiate(bullet,transform.position,transform.rotation);
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
     }
 }
