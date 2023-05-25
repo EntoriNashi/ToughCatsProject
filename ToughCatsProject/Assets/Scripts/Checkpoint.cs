b7de36800d21ba19b4d5c06505c30e06c58e71b4
Jonathan Jones <jpjones3@student.fullsail.edu>
2023-05-25 04:30:02 -0400

    UI, Checkpoints and Doors Oh My
    Changes to doors that allow dynamically spawning and despawn the unarmed scientist, which is a stopgate for a better fix.
    Updated checkpoints so that it will destroy itself after the player leaves the checkpoint, this is to prevent backwards progression
    UI prefab changed so that the damage flash does not cover up the menus

--- ToughCatsProject/Assets/Scripts/Checkpoint.cs
+++ ToughCatsProject/Assets/Scripts/Checkpoint.cs
@@ -7,13 +7,20 @@ public class Checkpoint : MonoBehaviour
     [SerializeField] Vector3 Position;
     [SerializeField] Quaternion Oriantation;
     [SerializeField] int SpawnHeightAdjustment;
-    
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Vector3 yAdjustment = new Vector3(0, SpawnHeightAdjustment, 0);
             gameManager.instance.UpdatePlayerSpawnPos(Position + yAdjustment, Oriantation);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Destroy(this);
         }
     }
 }
--- ToughCatsProject/Assets/Scripts/Door.cs
+++ ToughCatsProject/Assets/Scripts/Door.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     [SerializeField] Animator anim;
+
+    [SerializeField] bool IsRoomEnterence;
+    [SerializeField] bool IsRoomExit;
+    [SerializeField] GameObject UnarmedSpawnPos;
+    [SerializeField] GameObject Unarmed;
+
+    GameObject currentUnarmed;
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
             anim.SetBool("character_nearby", true);
+            if(IsRoomEnterence)
+            {
+                gameManager.instance.unarmed = Instantiate(Unarmed, UnarmedSpawnPos.transform.position, UnarmedSpawnPos.transform.rotation);
+                
+            }
         }
     }
 
@@ -19,6 +31,12 @@ public class Door : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             anim.SetBool("character_nearby", false);
+            if(IsRoomExit)
+            {
+                currentUnarmed = gameManager.instance.unarmed;
+                gameManager.instance.unarmed = null;
+                Destroy(currentUnarmed);
+            }
         }
     }
 }
